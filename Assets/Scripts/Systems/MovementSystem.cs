1e663ef682fc20c1669267ae1d240067ca7903e4
KeremYanlic <keremyanlc@outlook.com>
2024-05-04 14:10:13 +0300

    Commit for different version of system base

--- Assets/Scripts/Systems/MovementSystem.cs
+++ Assets/Scripts/Systems/MovementSystem.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
-public partial class MovementSystem : SystemBase
+public partial struct MovementSystem : ISystem
 {
-    protected override void OnUpdate()
+    public void OnCreate(ref SystemState systemState)
+    {
+
+    }
+
+    public void OnDestroy(ref SystemState systemState)
+    {
+
+    }
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
     {
         foreach(var (transform, speed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Speed>>())
         {
             //
         }
-        float deltaTime = World.Time.DeltaTime;
+        float deltaTime = state.World.Time.DeltaTime;
+
+        foreach(var (transform,speed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Speed>>()){
+            transform.ValueRW.Position += new float3(0, 0, speed.ValueRO.Value * deltaTime);
+        }
+
         // in, verilen parametrenin referans olarak verileceğini ve fonksiyon içerisinde değiştirilemeyeceğini gösterir
-        Entities.ForEach((ref LocalTransform transform, in Speed speed) =>
+        /*Entities.ForEach((ref LocalTransform transform, in Speed speed) =>
         {
             transform.Position += new float3(0, 0, speed.Value * deltaTime);
-        }).Run();
+        }).Run();*/
       
     }
 }
