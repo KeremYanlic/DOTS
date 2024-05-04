a4c47716e7e1f26a05668e416b35f89fab9c3cde
KeremYanlic <keremyanlc@outlook.com>
2024-05-04 12:30:57 +0300

    Size system has been updated

--- Assets/Scripts/Systems/SizeSystem.cs
+++ Assets/Scripts/Systems/SizeSystem.cs
@@ -10,9 +10,12 @@ public partial class SizeSystem : SystemBase
     
     protected override void OnUpdate()
     {
+        
+        float deltaTime = World.Time.DeltaTime;
+        
         Entities.ForEach((ref LocalTransform localTransform, in Size size) =>
         {
-            localTransform.Scale += size.sizeValue;
+            localTransform.Scale += size.sizeValue * deltaTime;
         }).Run();
     }
 }
